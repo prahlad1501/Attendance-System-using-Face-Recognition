d6cbd43e45e98526841444cb8a8f2272ae829739
Pranjal Rane <rane.pranjal18@vit.edu>
2019-11-28 17:06:41 +0530

    added attendance database

--- face_recog.py
+++ face_recog.py
@@ -1,8 +1,12 @@
 import face_recognition
 import cv2
 import numpy as np
+import time
 
-video_capture = cv2.VideoCapture(0)
+local_time=time.ctime(time.time())
+#print(local_time[2],type(local_time))
+
+n=15
 
 # Load a sample picture
 pranjal_image = face_recognition.load_image_file("pranjal.jpg")
@@ -19,70 +23,89 @@ prahlad_face_encoding = face_recognition.face_encodings(prahlad_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [pranjal_face_encoding, saurabh_face_encoding, tejas_face_encoding, prahlad_face_encoding]
-known_face_names = ["Pranjal", "saurabh", "tejas", "prahlad"]
+known_face_names = ["Pranjal", "Saurabh", "Tejas", "Prahlad"]
 
 # Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
 process_this_frame = True
-
 while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
+    video_capture = cv2.VideoCapture(0)
+    fps = video_capture.get(cv2. CAP_PROP_FPS)
+    count=0
+    present=set()
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    while True:
+        if (count==int(fps*n)):
+            break
+        #f = fopen ("attendance.txt", 'w')
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    # Only process every other frame of video to save time
-    if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
 
-        face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-            # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
 
-            face_names.append(name)
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+                    present.add(name)
 
-    process_this_frame = not process_this_frame
+                face_names.append(name)
+
+        process_this_frame = not process_this_frame
+        count+=1
 
 
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    # Display the resulting image
-    cv2.imshow('Video', frame)
+        # Display the resulting image
+        cv2.imshow('Video', frame)
 
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+    # Release handle to the webcam
+    cv2.destroyAllWindows()
+    local_time=time.ctime(time.time())
+    f=open("present_stud.txt",'a+')
+    f.write(str(local_time)+'\t')
+    f.write(str(list(present)))
+    f.write('\n\n')
+    f.close()
+    #print(1)
+    time.sleep(5)
+    #print(2)
+    video_capture.release()
